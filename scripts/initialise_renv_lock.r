e7d7646f49239f0fb0e33b0898f448e290a17016
christinaread-umcu <c.m.e.read-2@umcutrecht.nl>
2026-06-10 17:15:33 +0200

    added structure and wrote BMI function

---
+++ scripts/initialise_renv_lock.r
@@ -0,0 +1,12 @@
+#this script installs and loads necessary packages
+
+##install.packages("renv")
+##renv::init()
+renv::install()
+
+##install.packages(c("devtools", "roxygen2", "testthat"))
+
+##install.packages("rlang")
+
+##devtools::create(".")
+
