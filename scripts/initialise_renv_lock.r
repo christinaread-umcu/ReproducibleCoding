60d5de1c0cb41f1c2624efd8affa40015afaa2ec
christinaread-umcu <c.m.e.read-2@umcutrecht.nl>
2026-06-11 16:49:57 +0200

    updated description

--- scripts/initialise_renv_lock.r
+++ scripts/initialise_renv_lock.r
@@ -1,6 +1,7 @@
 #this script intialises the renv, then installs necessary packages
-
 install.packages("renv")
 renv::init()
 renv::install()
 
+#to new packages are added to the lockfile
+#renv::snapshot()
